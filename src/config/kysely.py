67c58de0020d54a54cb03f218918d87639f26dae
Elina <elina.ylkanen.gcp@gmail.com>
2021-11-02 11:33:58 +0200

    päivitystä

--- src/config/kysely.py
+++ src/config/kysely.py
@@ -2,21 +2,19 @@
 import psycopg2
 from config import config
 
-def kysely():
-    con = None
-    try:
-        con = psycopg2.connect(**config())
-        cursor = con.cursor()
-        SQL = 'SELECT * FROM person;'
-        cursor.execute(SQL)
-        row = cursor.fetchone()
-        print(row)
-        cursor.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if con is not None:
-            con.close()
 
-if __name__ == "__main__":
-    kysely()          
+con = None
+try:
+    con = psycopg2.connect(**config())
+    cursor = con.cursor()
+    SQL = 'SELECT * FROM person;'
+    cursor.execute(SQL)
+    row = cursor.fetchone()
+    print(row)
+    cursor.close()
+except (Exception, psycopg2.DatabaseError) as error:
+    print(error)
+finally:
+    if con is not None:
+        con.close()
+
