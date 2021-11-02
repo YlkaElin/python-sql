6f482957b1ef8162b96777269a1360cb58c088bf
Elina <elina.ylkanen.gcp@gmail.com>
2021-11-02 10:49:50 +0200

    päivitystä

--- src/data/queries.py
+++ src/data/queries.py
@@ -1,14 +1,19 @@
 
 import psycopg2
-import config as config
+from config.config import config
 
 def connect():
     con = None
     try:
         con = psycopg2.connect(**config())
+        print("Connection created")
     except (Exception,
     psycopg2.DatabaseError) as error:
         print(error)
     finally:
         if con is not None:
             con.close()
+
+if __name__ == "__main__":
+    connect()
+
