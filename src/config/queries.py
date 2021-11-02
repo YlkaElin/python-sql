685261be70ec22ce84efa038897ba7a5dd6d1a95
Elina <elina.ylkanen.gcp@gmail.com>
2021-11-02 10:14:42 +0200

    eka commit

---
+++ src/config/queries.py
@@ -0,0 +1,14 @@
+
+import psycopg2
+import src.config.config as config
+
+def connect():
+    con = None
+    try:
+        con = psycopg2.connect(**config())
+    except (Exception,
+    psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if con is not None:
+            con.close()
